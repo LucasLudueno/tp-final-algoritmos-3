d88dd6d9ab0eab30be0bc9ac454234425824a316
pabloino90 <pabloino90@gmail.com>
2014-06-03 19:06:21 +0000

    subo algunas correcciones

--- src/clasesTp/JugadorDetective.java
+++ src/clasesTp/JugadorDetective.java
@@ -5,8 +5,4 @@ public class JugadorDetective extends Jugador{
 	public JugadorDetective(Ciudad ciudad, int velocidad, int tiempo) {
 		super(ciudad, velocidad, tiempo);
 	}
-	
-	public String obtenerPistaDe(Aeropuerto aeropuerto) {
-		return aeropuerto.devolverPista(this);
-	}
 }
--- src/clasesTp/JugadorNovato.java
+++ src/clasesTp/JugadorNovato.java
@@ -5,8 +5,4 @@ public class JugadorNovato extends Jugador{
 	public JugadorNovato(Ciudad ciudad, int velocidad, int tiempo) {
 		super(ciudad, velocidad, tiempo);
 	}
-	
-	public String obtenerPistaDe(Aeropuerto aeropuerto) {
-		return aeropuerto.devolverPista(this);
-	}
 }
