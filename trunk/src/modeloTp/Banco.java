de732e45e2858ddcdccfdf5466e3bb9fb2ddb0b2
pabloino90@gmail.com <pabloino90@gmail.com@44e7fea7-e0d9-ed8b-f6c8-48a89e4760d4>
2014-06-08 19:37:19 +0000

    Faltaron agregar las clases nombradas en el commit anterior

---
+++ trunk/src/modeloTp/Banco.java
@@ -0,0 +1,9 @@
+package modeloTp;
+
+public class Banco extends Lugar {
+	
+	public Banco(Pista pistaFacil, Pista pistaMedia, Pista pistaDificil) {
+		super(pistaFacil, pistaMedia, pistaDificil);
+	}
+
+}
---
+++ trunk/src/modeloTp/Puerto.java
@@ -0,0 +1,8 @@
+package modeloTp;
+
+public class Puerto extends Lugar {
+	
+	public Puerto(Pista pistaFacil, Pista pistaMedia, Pista pistaDificil) {
+		super(pistaFacil, pistaMedia, pistaDificil);
+	}
+}
