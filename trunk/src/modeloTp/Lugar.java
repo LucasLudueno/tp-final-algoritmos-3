1fe77e84b36e0eaddc6021a58647176c4a63fccb
pabloino90@gmail.com <pabloino90@gmail.com@44e7fea7-e0d9-ed8b-f6c8-48a89e4760d4>
2014-06-08 18:28:36 +0000

    Cambio atributos y metodos publicos a protegidos en la clase Lugar

--- trunk/src/modeloTp/Lugar.java
+++ trunk/src/modeloTp/Lugar.java
@@ -2,11 +2,11 @@ package modeloTp;
 
 public class Lugar {
 	
-	private Pista pistaFacil;
-	private Pista pistaMedia;
-	private Pista pistaDificil;
+	protected Pista pistaFacil;
+	protected Pista pistaMedia;
+	protected Pista pistaDificil;
 	
-public Lugar(Pista pistaFacil, Pista pistaMedia, Pista pistaDificil){
+protected Lugar(Pista pistaFacil, Pista pistaMedia, Pista pistaDificil){
 		
 		this.pistaFacil = pistaFacil;
 		this.pistaMedia = pistaMedia;
@@ -14,19 +14,19 @@ public Lugar(Pista pistaFacil, Pista pistaMedia, Pista pistaDificil){
 
 	}
 	
-	public Pista devolverPista(JugadorNovato jugador) {
+	protected Pista devolverPista(JugadorNovato jugador) {
 		return this.pistaFacil;
 	}
 	
-	public Pista devolverPista(JugadorDetective jugador){	
+	protected Pista devolverPista(JugadorDetective jugador){	
 		return this.pistaMedia;
 	}
 	
-	public Pista devolverPista(JugadorInvestigador jugador){	
+	protected Pista devolverPista(JugadorInvestigador jugador){	
 		return this.pistaMedia;
 	}
 	
-	public Pista devolverPista(JugadorSargento jugador){	
+	protected Pista devolverPista(JugadorSargento jugador){	
 		return this.pistaDificil;
 	}	
 }
