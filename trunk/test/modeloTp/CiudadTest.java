22af022dc855aa6e4f4bdf70ef9297870304f268
luks93.racing@gmail.com <luks93.racing@gmail.com@44e7fea7-e0d9-ed8b-f6c8-48a89e4760d4>
2014-06-13 19:43:12 +0000

    Agrego el metodo obtenerLugares de ciudad (que el profe dijo que seguramente vamos a tener que usar) con las pruebas correspondientes.

--- trunk/test/modeloTp/CiudadTest.java
+++ trunk/test/modeloTp/CiudadTest.java
@@ -2,6 +2,7 @@ package modeloTp;
 
 import org.junit.Assert;
 import org.junit.Test;
+import java.util.ArrayList;
 
 import modeloTp.Ciudad;
 
@@ -10,14 +11,12 @@ public class CiudadTest {
 	@Test
 	public void deberiaCrearseLaCiudadConBuenosAiresComoNombreYPosicionYLugaresIngresados(){
 	
-		
 		Ciudad ciudad = new Ciudad("Buenos Aires",1,1,null,null,null);
 		
 		Assert.assertTrue(ciudad.obtenerNombre() == "Buenos Aires");
 		Assert.assertTrue(ciudad.obtenerPosicion_x() == 1);
 		Assert.assertTrue(ciudad.obtenerPosicion_y() == 1);
 		Assert.assertTrue(ciudad.cantidadLugares() == 3);
-		
 		
 	}
 	
@@ -28,6 +27,21 @@ public class CiudadTest {
 		Ciudad ciudad2 = new Ciudad("Londres",4,5,null,null,null);
 		
 		Assert.assertEquals(5, ciudad1.obtenerDistancia(ciudad2));
+	}
+	
+	@Test
+	public void obtenerLugaresDeberiaDevolverLosLugaresIngresados(){
+		Lugar aeropuerto = new Lugar("Aeropuerto",null, null, null);
+		Lugar bolsa = new Lugar("Bolsa",null, null, null);
+		Lugar biblioteca = new Lugar("Biblioteca",null, null, null);
+		Ciudad buenosAires = new Ciudad("Buenos Aires", 1, 1, aeropuerto, bolsa, biblioteca);
+		
+		ArrayList<Lugar> lugaresCiudad = buenosAires.obtenerLugares();
+		
+		Assert.assertTrue( lugaresCiudad.contains(aeropuerto) );
+		Assert.assertTrue( lugaresCiudad.contains(bolsa) );
+		Assert.assertTrue( lugaresCiudad.contains(biblioteca) );
+		
 	}
 	
 }
