7311b91c0f9938dd0eda2d0b95aeee2901e1689e
luks93.racing@gmail.com <luks93.racing@gmail.com@44e7fea7-e0d9-ed8b-f6c8-48a89e4760d4>
2014-06-14 16:44:06 +0000

    Subo nuevas pruebas de la clase ComputadoraPolicial, en un rato subo mas.

--- trunk/test/modeloTp/ComputadoraPolicialTest.java
+++ trunk/test/modeloTp/ComputadoraPolicialTest.java
@@ -9,7 +9,7 @@ public class ComputadoraPolicialTest {
 
 	@Test
 	public void laComputadoraPolicialDeberiaCrearseConLasCaracteristicasDelLadronYLosSospechososVacias(){
-		ComputadoraPolicial computadora = new ComputadoraPolicial();
+		ComputadoraPolicial computadora = new ComputadoraPolicial( new ArrayList<Ladron>() );
 		
 		Assert.assertTrue( computadora.obtenerSexoLadron() == null );
 		Assert.assertTrue( computadora.obtenerHobbyLadron() == null );
@@ -21,4 +21,58 @@ public class ComputadoraPolicialTest {
 		
 		Assert.assertTrue( sospechosos.size() == 0 );
 	}
+	
+	@Test 
+	public void laComputadoraDeberiaDevolverLasCaracteristicasDelLadronIngresadas(){
+		
+		ComputadoraPolicial computadora = new ComputadoraPolicial( new ArrayList<Ladron>() );
+		
+		computadora.agregarSexoLadron("masculino");
+		computadora.agregarHobbyLadron("football");
+		computadora.agregarVehiculoLadron("limusina");
+		computadora.agregarCabelloLadron("rojo");
+		computadora.agregarSeniaLadron("cicatriz");
+		
+		
+		Assert.assertTrue( computadora.obtenerSexoLadron() == "masculino" );
+		Assert.assertTrue( computadora.obtenerHobbyLadron() == "football" );
+		Assert.assertTrue( computadora.obtenerCabelloLadron() == "rojo" );
+		Assert.assertTrue( computadora.obtenerSeniaLadron() == "cicatriz" );
+		Assert.assertTrue( computadora.obtenerVehiculoLadron() == "limusina" );
+				
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*@Test
+	public void laComputadoraPolicialDeberiaDevolverUnaOrdenDeArrestoSiSoloUnSospechosoCoincideConLosDatosIngresados(){
+		
+		Ladron sospechoso1 = new Ladron("Pepe", "masculino", "tennis", "rojo", "joyas", "limusina");
+		Ladron sospechoso2 = new Ladron("Maria", "femenino", "handball", "castanio", "cicatriz", "limusina");
+		Ladron ladron = new Ladron("Jose", "masculino", "football", "negro", "ninguna", "desacapotable");
+		ArrayList<Ladron> sospechosos = new ArrayList<Ladron>();
+		sospechosos.add(sospechoso1);
+		sospechosos.add(sospechoso2);
+		sospechosos.add(ladron);
+		
+		ComputadoraPolicial computadora = new ComputadoraPolicial(sospechosos);
+		
+		computadora.agregarSexoLadron("masculino");
+		computadora.agregarHobbyLadron("football");
+		computadora.agregarVehiculoLadron("limusina");
+		
+		OrdenDeArresto orden = computadora.emitirOrdenDeArresto();
+		
+		Assert.assertTrue( orden.obtenerNombreLLadron() == "Jose" );
+		
+	} */
 }
