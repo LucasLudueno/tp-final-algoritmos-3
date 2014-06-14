ade6f1da74f1273597479b7a83e7e4ee72ba2d7b
luks93.racing <luks93.racing@gmail.com>
2014-06-14 18:47:42 +0000

    Subo algunas pruebas mas de ComputadoraPolicial. Deje unas pruebas encerradas entre comentarios para que no corran, ya que aun no son necesarias y quizas ni las utilicemos.

--- test/modeloTp/ComputadoraPolicialTest.java
+++ test/modeloTp/ComputadoraPolicialTest.java
@@ -6,7 +6,51 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class ComputadoraPolicialTest {
-
+	
+	@Test
+	public void buscarPosiblesLadronesDeberiaDevolverUnaListaConUnLadronSiSoloUnoCoincideConLasCarateristicasIndicadas(){
+		
+		Ladron sospechoso1 = new Ladron("Pepe", "masculino", "tennis", "rojo", "joyas", "limusina");
+		Ladron sospechoso2 = new Ladron("Maria", "femenino", "handball", "castanio", "cicatriz", "limusina");
+		Ladron ladron = new Ladron("Jose", "masculino", "football", "negro", "ninguna", "limusina");
+		ArrayList<Ladron> sospechosos = new ArrayList<Ladron>();
+		sospechosos.add(sospechoso1);
+		sospechosos.add(sospechoso2);
+		sospechosos.add(ladron);
+		
+		ComputadoraPolicial computadora = new ComputadoraPolicial(sospechosos);
+		
+		ArrayList<Ladron> posiblesLadrones = computadora.buscarPosiblesLadrones("masculino", "football", null, null, "limusina");
+		
+		Assert.assertTrue( posiblesLadrones.size() == 1 );
+		Assert.assertTrue( ( ((Ladron)posiblesLadrones.get(0)).obtenerNombre() ) == ladron.obtenerNombre() );
+		
+	}
+	
+	@Test
+	public void buscarPosiblesLadronesDeberiaDevolverUnaListaConLosLadronesQueCoincideConLasCarateristicasIndicadas(){
+		
+		Ladron sospechoso1 = new Ladron("Pepe", "masculino", "tennis", "rojo", "joyas", "limusina");
+		Ladron sospechoso2 = new Ladron("Maria", "femenino", "handball", "castanio", "cicatriz", "limusina");
+		Ladron sospechoso3 = new Ladron("Jose", "masculino", "football", "negro", "ninguna", "limusina");
+		ArrayList<Ladron> sospechosos = new ArrayList<Ladron>();
+		sospechosos.add(sospechoso1);
+		sospechosos.add(sospechoso2);
+		sospechosos.add(sospechoso3);
+		
+		ComputadoraPolicial computadora = new ComputadoraPolicial(sospechosos);
+		
+		ArrayList<Ladron> posiblesLadrones = computadora.buscarPosiblesLadrones( null, null , null, null, "limusina");
+		
+		Assert.assertTrue( posiblesLadrones.size() == 3 );
+		Assert.assertTrue( posiblesLadrones.contains(sospechoso1) );
+		Assert.assertTrue( posiblesLadrones.contains(sospechoso2) );
+		Assert.assertTrue( posiblesLadrones.contains(sospechoso3) );
+		
+	}
+	
+	
+	/*
 	@Test
 	public void laComputadoraPolicialDeberiaCrearseConLasCaracteristicasDelLadronYLosSospechososVacias(){
 		ComputadoraPolicial computadora = new ComputadoraPolicial( new ArrayList<Ladron>() );
@@ -41,38 +85,7 @@ public class ComputadoraPolicialTest {
 		Assert.assertTrue( computadora.obtenerVehiculoLadron() == "limusina" );
 				
 	}
+	*/
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	/*@Test
-	public void laComputadoraPolicialDeberiaDevolverUnaOrdenDeArrestoSiSoloUnSospechosoCoincideConLosDatosIngresados(){
-		
-		Ladron sospechoso1 = new Ladron("Pepe", "masculino", "tennis", "rojo", "joyas", "limusina");
-		Ladron sospechoso2 = new Ladron("Maria", "femenino", "handball", "castanio", "cicatriz", "limusina");
-		Ladron ladron = new Ladron("Jose", "masculino", "football", "negro", "ninguna", "desacapotable");
-		ArrayList<Ladron> sospechosos = new ArrayList<Ladron>();
-		sospechosos.add(sospechoso1);
-		sospechosos.add(sospechoso2);
-		sospechosos.add(ladron);
-		
-		ComputadoraPolicial computadora = new ComputadoraPolicial(sospechosos);
-		
-		computadora.agregarSexoLadron("masculino");
-		computadora.agregarHobbyLadron("football");
-		computadora.agregarVehiculoLadron("limusina");
-		
-		OrdenDeArresto orden = computadora.emitirOrdenDeArresto();
-		
-		Assert.assertTrue( orden.obtenerNombreLLadron() == "Jose" );
-		
-	} */
 }
