743608b6d6eb4265079f6b1ab6df80e5260fe674
luks93.racing@gmail.com <luks93.racing@gmail.com@44e7fea7-e0d9-ed8b-f6c8-48a89e4760d4>
2014-06-14 18:47:42 +0000

    Subo algunas pruebas mas de ComputadoraPolicial. Deje unas pruebas encerradas entre comentarios para que no corran, ya que aun no son necesarias y quizas ni las utilicemos.

--- trunk/src/modeloTp/ComputadoraPolicial.java
+++ trunk/src/modeloTp/ComputadoraPolicial.java
@@ -4,71 +4,34 @@ import java.util.ArrayList;
 
 public class ComputadoraPolicial {
 
-	private String sexoLadron;
-	private String cabelloLadron;
-	private String hobbyLadron;
-	private String seniaLadron;
-	private String vehiculoLadron;
 	private ArrayList<Ladron> sospechosos;
 	
 	public ComputadoraPolicial(ArrayList<Ladron> sospechosos){
 	
 		this.sospechosos = sospechosos;
 	}
-	
-	public String obtenerSexoLadron() {
-		
-		return this.sexoLadron;
-	}
 
-	public String obtenerHobbyLadron() {
+	public ArrayList<Ladron> buscarPosiblesLadrones(String sexo, String hobby, String cabello, String senia, String vehiculo) {
 		
-		return this.hobbyLadron;
-	}
-
-	public String obtenerCabelloLadron() {
+		ArrayList<Ladron> posiblesLadrones = new ArrayList<Ladron>();
+		for(Ladron sospechoso: this.sospechosos){
+	          
+			if ( (sexo == null) || ( sexo == sospechoso.obtenerSexo() ) ){
+				if( (hobby == null) || ( hobby == sospechoso.obtenerHobby() ) ){
+					if( (cabello == null) || ( cabello == sospechoso.obtenerCabello() ) ){
+						if( (senia == null) || ( senia == sospechoso.obtenerSenia() ) ){
+							if( (vehiculo == null) || ( vehiculo == sospechoso.obtenerVehiculo() ) ){
+								
+								posiblesLadrones.add(sospechoso);
+							}
+						}
+					}
+				}	
+			}				
+		}
 		
-		return this.cabelloLadron;
-	}
-
-	public String obtenerSeniaLadron() {
+		return posiblesLadrones;
 		
-		return this.seniaLadron;
-	}
-
-	public String obtenerVehiculoLadron() {
-		
-		return this.vehiculoLadron;
-	}
-
-	public ArrayList<Ladron> obtenerSospechosos() {
-		
-		return this.sospechosos;
-	}
-
-	public void agregarSexoLadron(String sexoLadron) {
-		
-		this.sexoLadron = sexoLadron;
-	}
-
-	public void agregarHobbyLadron(String hobbyLadron) {
-		
-		this.hobbyLadron = hobbyLadron;		
-	}
-
-	public void agregarVehiculoLadron(String vehiculoLadron) {
-		
-		this.vehiculoLadron = vehiculoLadron;
-	}
-
-	public void agregarCabelloLadron(String cabelloLadron) {
-		
-		this.cabelloLadron = cabelloLadron;		
-	}
-
-	public void agregarSeniaLadron(String seniaLadron) {
-		
-		this.seniaLadron = seniaLadron;		
 	}
 
 }
