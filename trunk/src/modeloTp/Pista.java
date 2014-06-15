65eac87b46571113951ab8e71da5df8ecaad7cbc
pabloino90@gmail.com <pabloino90@gmail.com@44e7fea7-e0d9-ed8b-f6c8-48a89e4760d4>
2014-06-15 05:08:22 +0000

    Agrego los metodos para serializar y cargar estado mediante XML en la clase Pista con sus respectivos test

--- trunk/src/modeloTp/Pista.java
+++ trunk/src/modeloTp/Pista.java
@@ -1,14 +1,31 @@
 package modeloTp;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 public class Pista {
 	
 	private String contenido;
 	
 	public Pista(String unTexto) {
-		contenido = unTexto;
+		this.contenido = unTexto;
 	}
 
 	public String obtenerContenido() {
 		return this.contenido;
 	}
+
+	public Element serializar(Document doc) {
+		Element element = doc.createElement("Pista");
+		element.setAttribute("contenido", this.contenido);
+		return element;
+	}
+
+	public static Pista cargarEstado(Document doc) {
+		Pista nuevaPista = new Pista("");
+		Element elementoPista = (Element)doc.getElementsByTagName("Pista").item(0);
+		nuevaPista.contenido = elementoPista.getAttribute("contenido");
+		return nuevaPista;
+	}
+	
 }
--- trunk/test/modeloTp/PistaTest.java
+++ trunk/test/modeloTp/PistaTest.java
@@ -1,9 +1,30 @@
 package modeloTp;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 
 import modeloTp.Pista;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 public class PistaTest {
 	
@@ -13,6 +34,55 @@ public class PistaTest {
 		Pista pista = new Pista("unTexto");
 		
 		Assert.assertTrue(pista.obtenerContenido() == "unTexto");
+		
+	}
+	
+	@Test
+	public void pistaDeberiaGuardarYRecuperarATravesDeXML() throws ParserConfigurationException, TransformerException, SAXException, IOException{
+		
+		//Genero un documento XML vacio en la memoria
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		Document doc = db.newDocument();	
+		
+		Pista unaPista = new Pista("Contenido de pista");
+		
+		//Asigno el elemento XML de la clase Pista al documento generado anteriormente
+		Element pistaSerializada = unaPista.serializar(doc);
+		
+		assertNotNull(pistaSerializada);
+		
+		
+		//Guardo el XML en el disco
+		doc.appendChild(pistaSerializada);
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+		DOMSource source = new DOMSource(doc);
+		File archivo = new File("pistaTest.xml");
+		StreamResult streamResult = new StreamResult(archivo);
+		transformer.transform(source, streamResult);
+		
+		assertTrue(archivo.exists());
+		
+		
+		//Ahora recupero el estado guardado de pista en una nueva instancia
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		doc = dBuilder.parse(archivo);
+		doc.getDocumentElement().normalize();
+		
+		Pista otraPista = Pista.cargarEstado(doc);
+		assertNotNull(otraPista);
+		assertEquals(unaPista.obtenerContenido(), otraPista.obtenerContenido());
+				
+	}
+	
+	@After
+	public void after(){
+		File archivo = new File("pistaTest.xml");
+		if (archivo.exists()){
+			archivo.delete();
+		}
 		
 	}
 
