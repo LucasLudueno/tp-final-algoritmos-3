b896649588eb1345ce1cf53d5b4f69714a21a754
luks93.racing@gmail.com <luks93.racing@gmail.com@44e7fea7-e0d9-ed8b-f6c8-48a89e4760d4>
2014-06-21 15:02:23 +0000

    Subo al generador de partidas la generacion de la lista de los ladrones y la clase para serializar la lista de los ladrones.

--- trunk/src/modeloTp/Juego.java
+++ trunk/src/modeloTp/Juego.java
@@ -1,10 +1,29 @@
 package modeloTp;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import modeloTp.Ladron;
+import modeloTp.GeneradorDePartidas;
+
 public class Juego {
 	
-	
-	public void iniciarJuego(){
+	@Test
+	public void iniciarJuego() throws ParserConfigurationException, TransformerException, SAXException, IOException{
 		
+		GeneradorDePartidas generador = new GeneradorDePartidas();
+		
+		ArrayList<Ladron> ladrones = generador.generarListaDeLadrones();
+		Assert.assertTrue( true);
 	}
 	
 
