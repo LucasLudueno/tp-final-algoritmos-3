217c5dbaf08a7308cf66e74f2a51194439e3ba65
luks93.racing@gmail.com <luks93.racing@gmail.com@44e7fea7-e0d9-ed8b-f6c8-48a89e4760d4>
2014-06-22 15:10:35 +0000

    Redondie la carga del juego. Ya se puede empezar a implementar la interfaz.

--- trunk/src/modeloTp/Juego.java
+++ trunk/src/modeloTp/Juego.java
@@ -4,21 +4,33 @@ package modeloTp;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
+
 //import java.util.ArrayList;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 
-//import modeloTp.Ladron;
 //import modeloTp.GeneradorDePartidas;
 
 public class Juego {
 	
 	public void iniciarJuego() throws ParserConfigurationException, TransformerException, SAXException, IOException{
 		
-		//GeneradorDePartidas generador = new GeneradorDePartidas(); ESTO SI VA
+		/* ACA SE CARGAN TODOS LOS DATOS DEL JUEGO
+		 
+		GeneradorDePartidas generador = new GeneradorDePartidas();
+		generador.cargarDatos();
+		ArrayList<Ciudad> recorridoDelLadron = generador.obtenerRecorridoDelLadron();
+		ArrayList<Ciudad> ciudadesAuxiliares = generador.obtenerCiudadesAuxiliares();
+		ArrayList<Ladron> sospechosos = generador.generarListaDeLadrones();
 		
-		//ArrayList<Ladron> ladrones = generador.generarListaDeLadrones(); ESTO SI VA
+		ComputadoraPolicial computadoraJugador = new ComputadoraPolicial(sospechosos);
+		Ciudad ciudadActual = recorridoDelLadron.get(0);
+		
+		JugadorNovato jugador = new JugadorNovato(ciudadActual, computadoraJugador);
+		
+		*/
+		
 	}
 	
 
